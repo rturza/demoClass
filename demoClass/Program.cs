9b7c01c51dba8fe8533d7f0d03ade243a9b543f6
Turza <cse.turza@gmail.com>
2022-06-28 17:01:29 +0600

    dividingClass

--- demoClass/Program.cs
+++ demoClass/Program.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace demoClass
 {
-    public class person
-    {
-        public string FirstName;
-        public string LastName;
-
-        public void Introduce()
-        {
-            Console.WriteLine("My Name is " + FirstName + " " + LastName);
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
---
+++ demoClass/person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace demoClass
+{
+    public class person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My Name is " + FirstName + " " + LastName);
+        }
+    }
+}
