df5ae654b747761ee65cc469bcc538559ca935e7
Turza <cse.turza@gmail.com>
2022-06-28 16:53:12 +0600

    Add project files.

---
+++ demoClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoClass
+{
+    public class person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My Name is " + FirstName + " " + LastName);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person john = new person();
+            john.FirstName = "John";
+            john.LastName = "Doe";
+            john.Introduce();
+
+        }
+    }
+}
